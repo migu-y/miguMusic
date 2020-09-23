7a9b1886048b36d1c12e7edf1a4232c3def39f50
Sefan <962007314@qq.com>
2020-09-23 21:27:46 +0800

    init

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    // 插件的集合
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    // 独立安装的插件
+    plugins: [
+        ['import', {
+            libraryName: 'vant',
+            libraryDirectory: 'es',
+            style: true
+        }, 'vant']
+    ]
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
+
+// 屏蔽开发环境的提示
 Vue.config.productionTip = false
 
+// 引入全局样式
+import './assets/reset.css'
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
