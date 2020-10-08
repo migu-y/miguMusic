133efcdeb771cf5bc60ad2c05ab4e7b19828ed09
Sefan <962007314@qq.com>
2020-10-09 07:54:26 +0800

    final

--- sefan/src/components/GoodsDetails.vue
+++ sefan/src/components/GoodsDetails.vue
@@ -24,7 +24,7 @@
                     <span>喜茶</span> 
                 </div>
                 <van-address-edit
-
+                    :area-list="areaList"
                 />
                 <div class="brand">
                     <span>运费</span> 
@@ -73,6 +73,29 @@ Vue.use(AddressEdit);
 export default {
     data() {
         return {
+            areaList:{
+  province_list: {
+    110000: '北京市',
+    120000: '天津市'
+  },
+  city_list: {
+    110100: '北京市',
+    110200: '县',
+    120100: '天津市',
+    120200: '县'
+  },
+  county_list: {
+    110101: '东城区',
+    110102: '西城区',
+    110105: '朝阳区',
+    110106: '丰台区',
+    120101: '和平区',
+    120102: '河东区',
+    120103: '河西区',
+    120104: '南开区',
+    120105: '河北区',
+  }
+},
             show:false,
             goods:this.$router.history.current.params
         };
--- sefan/src/views/order/historyOrder/HistoryOrder.vue
+++ sefan/src/views/order/historyOrder/HistoryOrder.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="history">
         <van-tabs type="card" v-model="active">
-            <van-tab title="门店订单">没有门店</van-tab>
+            <!-- <van-tab title="门店订单">没有门店</van-tab> -->
             <van-tab title="百货订单"><VarietyOrder></VarietyOrder> </van-tab>
-            <van-tab title="批量开票">没有门店咋开票</van-tab>
+            <van-tab title="批量开票"> 请稍候</van-tab>
         </van-tabs>
     </div>
 </template>
